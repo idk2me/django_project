f1839cebeef05e51b9564bf67e914ab70b587685
Patrikas Gabrielius Ionescu <patrickgabrielionescu@gmail.com>
2024-01-31 15:58:08 +0100

    added templates and styling

--- app/app/settings.py
+++ app/app/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'some_app/static/templates', BASE_DIR/'some_app/static/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- app/some_app/views.py
+++ app/some_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+# create an index view
+
+def index(request):
+    return render(request, 'index.html')
