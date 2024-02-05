914002f5e1d48dbfb0fd6071f0506cd736b395eb
Patrikas Gabrielius Ionescu <patrickgabrielionescu@gmail.com>
2024-02-05 11:00:12 +0100

    requirements.txt

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,10 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 ADMINS = [str(os.getenv('EMAIL'))]
